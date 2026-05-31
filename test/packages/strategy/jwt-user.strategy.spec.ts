70f621c68d54e4acf504ae041f9bf59027e261d7
Your Name <you@example.com>
2026-05-31 16:50:50 +0700

    init test config

--- test/packages/strategy/jwt-user.strategy.spec.ts
+++ test/packages/strategy/jwt-user.strategy.spec.ts
@@ -1,3 +1,32 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtUserStrategy } from '@packages/strategy';
+
 describe('JwtUserStrategy', () => {
-  it.todo('should be implemented');
+  let configService: ConfigService;
+
+  beforeEach(() => {
+    configService = {
+      get: jest.fn(),
+    } as unknown as ConfigService;
+  });
+
+  describe('init constructor and check data from this ...', () => {
+    it('should be create constructor and check data from this ...', () => {
+      jest.spyOn(configService, 'get').mockImplementation((key: string) => {
+        return {
+          JWT_ACCESS_SECRET: 'test-secret',
+        }[key];
+      });
+      const strategy = new JwtUserStrategy(configService);
+
+      expect(strategy).toBeDefined();
+      expect((configService.get as jest.Mock).mock.calls?.[0]?.[0] as unknown as string).toBe(
+        'JWT_ACCESS_SECRET',
+      );
+    });
+  });
+});
+
+describe('init configService and data from this ...', () => {
+  it('should be implemented', () => {});
 });
